4d6dd4cceacd3c5d3982d9eec0c44264fda5c131
IvanIlchenko795 <sok16777@gmail.com>
2025-02-13 18:19:48 +0200

    Lab-2

--- Lab-2/CandyCostCalculator.java
+++ Lab-2/CandyCostCalculator.java
@@ -3,23 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CandyCostCalculator {
-
     public static void main(String[] args) throws IOException {
         // Створення об'єкта BufferedReader для введення даних
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         // Введення даних для шоколадних цукерок
-        System.out.println("Введіть кількість кг шоколадних цукерок (X):");
+        System.out.println("Enter the amouth of chocolates kg (X):");
         double chocolateWeight = Double.parseDouble(reader.readLine());
 
-        System.out.println("Введіть загальну ціну шоколадних цукерок (A):");
+        System.out.println("Enter the total prise of the chocolates (A):");
         double chocolateTotalCost = Double.parseDouble(reader.readLine());
 
         // Введення даних для ірисок
-        System.out.println("Введіть кількість кг ірисок (Y):");
+        System.out.println("Enter the amouth of toffee kg (Y):");
         double toffeeWeight = Double.parseDouble(reader.readLine());
 
-        System.out.println("Введіть загальну ціну ірисок (B):");
+        System.out.println("Enter the total prise of the toffee (B):");
         double toffeeTotalCost = Double.parseDouble(reader.readLine());
 
         // Розрахунок ціни за 1 кг шоколадних цукерок
@@ -32,8 +31,8 @@ public class CandyCostCalculator {
         double priceDifference = chocolateCostPerKg / toffeeCostPerKg;
 
         // Виведення результатів
-        System.out.printf("Ціна за 1 кг шоколадних цукерок: %.2f грн%n", chocolateCostPerKg);
-        System.out.printf("Ціна за 1 кг ірисок: %.2f грн%n", toffeeCostPerKg);
-        System.out.printf("Шоколадні цукерки дорожчі за іриски у %.2f рази%n", priceDifference);
+        System.out.printf("Prise for 1 kg of chocolates: %.2f usd%n", chocolateCostPerKg);
+        System.out.printf("Prise for 1 kg of toffee: %.2f usd%n", toffeeCostPerKg);
+        System.out.printf("Chocolates are more expensive than toffees in %.2f as%n", priceDifference);
     }
 }

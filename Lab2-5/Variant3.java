9355c67b596e4d08f3ad9987b84982c541b57846
IvanIlchenko795 <sok16777@gmail.com>
2025-04-08 18:07:28 +0300

    Laba_2-5

---
+++ Lab2-5/Variant3.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class Variant3 {
+    public static void main(String[] args) {
+        int n = 5; // Кількість рядків
+        int m = 5; // Кількість стовпців
+        int[][] Z = new int[n][m];
+        Random rand = new Random();
+
+        int a = 2, b = 10; // Межі для фільтрації
+        int product = 1;
+        int sum = 0, count = 0;
+
+        System.out.println("Масив Z:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Z[i][j] = rand.nextInt(21) - 6; // Випадкові числа [-6; 14]
+                System.out.print(Z[i][j] + "\t");
+
+                // Середнє арифметичне додатних
+                if (Z[i][j] > 0) {
+                    sum += Z[i][j];
+                    count++;
+                }
+
+                // Добуток для парних рядків
+                if (i % 2 == 1 && Z[i][j] > a && Z[i][j] < b) {
+                    product *= Z[i][j];
+                }
+            }
+            System.out.println();
+        }
+
+        // Середнє арифметичне
+        double avgPositive = count > 0 ? (double) sum / count : 0;
+
+        // Виведення результатів
+        System.out.println("\nДобуток елементів з парних рядків (" + a + "; " + b + "): " + product);
+        System.out.println("Середнє арифметичне додатних елементів: " + avgPositive);
+    }
+}
